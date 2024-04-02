d329181fa0fa9292a7c7cfc856ca97f59594aee4
cmkuczun <93489352+cmkuczun@users.noreply.github.com>
2024-04-02 15:59:43 -0400

    added header col

--- clean_data.py
+++ clean_data.py
@@ -8,10 +8,14 @@ clean = open(clean_csv, 'w', newline='')
 
 with open(orig_csv, 'r') as orig:
     lines = orig.readlines()
+    count = 0
     for line in lines:
+        if count == 0:
+            clean.write(line)
         if line[0].isdigit():
             print(line)
             clean.write(line)
+        count += 1
 
 # for each original row, if the row starts with "a number,"keep it 
 # write it to the clean csv file
